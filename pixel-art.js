f7aaf9f3c4fd148b7bbd6598bfabc10a8d65121f
CHANSE HORTON <chansehorton@yahoo.com>
2016-10-19 21:40:30 -0600

    committing with all basic functionality met

--- pixel-art.js
+++ pixel-art.js
@@ -4,15 +4,18 @@ window.onload = function() {
   var currentColor = 'white';
   var pixelTable = document.querySelector(".pixel_table");
   var colorTable = document.querySelector(".color_table");
-  var colorArray = ['#FF355E','#FD5B78','#FF6037','#FF9966','#FF9933','#FFCC33','#FFFF66','#FFFF66','#CCFF00','#66FF66','#AAF0D1','#50BFE6','#FF6EFF','#FF6EFF','#EE34D2','#FF00CC'];
+  var colorArray = ['#FF355E','#FD5B78','#FF6037','#FF9966','#FF9933','#FFCC33','#FFFF66','#FFFF66','#CCFF00','#66FF66','#AAF0D1','#50BFE6','#FF6EFF','#FF6EFF','#EE34D2','#FF00CC', '#222', '#FFFFFF'];
 
 
   colorTable.addEventListener('click', setColor, false);
   pixelTable.addEventListener('click', changeColor, false);
 
-  populatePixels(40,100);
+
+  populatePixels(48,100);
+  populateColorTable(colorArray);
 
   function changeColor(e) {
+    console.log(e.target.className);
     if (e.target !== e.currentTarget) {
       var clickedItem = e.target;
       clickedItem.style.backgroundColor = currentColor;
@@ -24,6 +27,7 @@ window.onload = function() {
     if (e.target !== e.currentTarget) {
       var clickedItem = e.target;
       currentColor = window.getComputedStyle(clickedItem).getPropertyValue('background-color');
+      document.querySelector(".selected_color").style.backgroundColor = currentColor;
     };
     e.stopPropagation();
   }
@@ -31,11 +35,13 @@ window.onload = function() {
   function populatePixels(rows, columns) {
     for (var i=0;i<=rows;i++) {
       var newRow = document.createElement('div');
+
       newRow.className = 'row';
       pixelTable.appendChild(newRow);
 
       for (var j=0;j<=columns;j++) {
         var newPixel = document.createElement('span');
+
         newPixel.className = 'pixel';
         newRow.appendChild(newPixel);
       }
@@ -43,6 +49,25 @@ window.onload = function() {
   }
 
   function populateColorTable(colors) {
-    for (var i=0;i<)
-  }
+    var colorRow1 = document.createElement('div');
+    var colorRow2 = document.createElement('div');
+
+    colorRow1.className = 'color_row';
+    colorRow2.className = 'color_row';
+
+    for (var i=0;i<colors.length;i++) {
+      var newColor = document.createElement('span');
+
+      newColor.className = 'color_choice';
+      newColor.style.backgroundColor = colors[i];
+
+      if (i<(colors.length/2)) {
+        colorRow1.appendChild(newColor);
+      } else {
+        colorRow2.appendChild(newColor);
+      };
+    };
+    colorTable.appendChild(colorRow1);
+    colorTable.appendChild(colorRow2);
+  };
 };
